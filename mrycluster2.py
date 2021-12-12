8263b44c00ffb58980f1e5c03f6d1148053528bb
Anssi Laukkarinen <anssi.laukkarinen@tuni.fi>
2021-12-12 22:55:33 +0200

    Combine different codes to one folder

--- mrycluster2.py
+++ mrycluster2.py
@@ -11,9 +11,19 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+
+
+import mry_helper
+
 import scipy.cluster.hierarchy as sch
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import DBSCAN
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.utils.fixes import loguniform
 
 
 
@@ -34,154 +44,111 @@ with open(fname, 'rb') as f:
 
 ################
 
-def create_X(data, case_filters, y, y_not):
-
-    list_values = []
-    list_names = []
-    
-    for idx_case, case in enumerate(data):
-        
-        # Check that location, climate and year columns are identical
-        if idx_case == 0:
-            ids1 = data[case].loc[:, ['location', 'climate', 'year']].copy()
-        else:
-            ids2 = data[case].loc[:, ['location', 'climate', 'year']].copy()
-            
-            if not ids1.equals(ids2):
-                print('NOT EQUAL:', case)
-            
-        
-        # continue
-        for idx_column, column in enumerate(data[case].columns):
-            
-            cond1 = all(x in case for x in case_filters)
-            cond2 = y in column and y_not not in column
-            if cond1 and cond2:
-                
-                column_str = '{}__{}'.format(case, column)
-                
-                list_values.append(data[case].loc[:, column])
-                list_names.append(column_str)
-    
-    df_X = pd.concat(list_values, axis=1, keys=list_names)
-    df_X = pd.concat([df_X, ids1], axis=1)
-    
-    return(df_X)
 
 
 
 
-
-def generate_30year_plots(df, output_folder):
+def cluster_and_export(df, ml_args, output_folder, fname_str):
     
-    labels_for_median = ['1989-2018', 'RCP45-2050','RCP45-2080','RCP85-2050','RCP85-2080']
-    
-    idxs_col = ['location', 'climate', 'year']
-    ids = df[idxs_col].copy()
-    df = df.drop(columns=idxs_col).copy()
-    
-    locations = ids['location'].unique()
-    
-    
-    
-    ### UPDATE medians.xlsx file name
-    
-    xlsx_file_path = os.path.join(output_folder, 'medians.xlsx')
-    writer = pd.ExcelWriter(xlsx_file_path, engine='xlsxwriter')
-    
-    for location in locations:
-        
-        print('location:', location)
-        
-        # calculate original means and ranked values
-        idxs_rows = ids.loc[:, 'location'] == location
-        
-        y = df.loc[idxs_rows, :].mean(axis=1).values.reshape((30, -1), order='F')
-        x = ids.loc[idxs_rows, 'year'].values[0:30].reshape(-1)
-        climates = ids.loc[idxs_rows, 'climate'].unique()
-        
-        y_df = pd.DataFrame(y, index=x, columns=climates)
-        y_df_ranked = y_df.rank(axis=0)
-        
-        y_df.loc['mean'] = y_df.mean(axis=0)
-        y_df.round(2).to_excel(writer, sheet_name=location)
-        
-        y_df_ranked.loc[:, 'median'] = y_df_ranked.loc[:, labels_for_median].median(axis=1)
-        y_df_ranked.to_excel(writer, sheet_name=location+'_ranked')
-        
-        
-        # make plots
-        # ranks of different years 1989-2018
-        # scatter plots of original values as a function of year
-        
-    writer.save()
-        
-
-
-
-
-def cluster_and_export_agglomerative(df, output_folder, fname_str):
+    df_local = df.copy()
     
     # Drop non-numerical columns
     idxs_col = ['location', 'climate', 'year']
-    ids = df[idxs_col].copy()
-    df.drop(columns=idxs_col, inplace=True)    
+    ids = df_local[idxs_col].copy()
+    df_local.drop(columns=idxs_col, inplace=True)
+    #print('cluster_and_export df.shape, this contains all case+mp pairs:', df_local.shape)
     
+    if ml_args['n_clusters'] > df_local.shape[1]:
+        ml_args['n_clusters'] = df_local.shape[1]
     
     #fit model
-    model = AgglomerativeClustering(n_clusters=2,
-                                    affinity='manhattan',
-                                    linkage='average')    
-    model.fit(df.T)
+    if ml_args['name'] == 'agglomerative':
+        model = AgglomerativeClustering(n_clusters=ml_args['n_clusters'],
+                                        affinity=ml_args['affinity'],
+                                        linkage=ml_args['linkage'])
+        model.fit(df_local.T)
+        output_dir = os.path.join(output_folder,
+                                  ml_args['name'] + '_' + ml_args['affinity'] + '_' + ml_args['linkage'],
+                                  'n_clusters_max{}'.format(ml_args['n_clusters']))
     
-    # export column names (and values) to excel
-    cols0 = df.columns[model.labels_ == 0]
-    cols1 = df.columns[model.labels_ == 1]
+    elif ml_args['name'] == 'kmeans':
+        model = KMeans(n_clusters=ml_args['n_clusters'],
+                       n_init=ml_args['n_init'],
+                       max_iter=ml_args['max_iter'],
+                       tol=ml_args['tol'],
+                       verbose=0)
+        model.fit(df_local.T)
+        output_dir = os.path.join(output_folder,
+                                  'kmeans',
+                                  'n_clusters_max{}'.format(ml_args['n_clusters']))
     
-    df0 = df.loc[:, cols0].copy()
-    df1 = df.loc[:, cols1].copy()
     
-    df0 = pd.concat([ids, df0], axis=1)
-    df1 = pd.concat([ids, df1], axis=1)
+    elif ml_args['name'] == 'spectralclustering':
+        model = SpectralClustering(n_clusters=ml_args['n_clusters'])
+        model.fit(df_local.T)
+        output_dir = os.path.join(output_folder,
+                                  'spectralclustering',
+                                  'n_clusters_max{}'.format(ml_args['n_clusters']))
     
-    output_dir = os.path.join(output_folder, 'Agglomerative')
+    elif ml_args['name'] == 'dbscan':
+        model = DBSCAN(eps=ml_args['eps'],
+                           min_samples=ml_args['min_samples'],
+                           leaf_size=ml_args['leaf_size'])
+        model.fit(df_local.T)
+        output_dir = os.path.join(output_folder,
+                                  'dbscan',
+                                  'eps_{:.2f}'.format(ml_args['eps']))
+        
+        
+        
+        
+    
+    
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-        
-    xlsx_file_path = os.path.join(output_dir, fname_str + '.xlsx')
     
+    # export column names (and values) to excel
+    xlsx_file_path = os.path.join(output_dir, fname_str + '_cases1200.xlsx')
     writer = pd.ExcelWriter(xlsx_file_path, engine='xlsxwriter')
-    df0.T.to_excel(writer, sheet_name='df0')
-    df1.T.to_excel(writer, sheet_name='df1')
+    list_df = []
+    
+    for label_idx in range(np.max(model.labels_) + 1):
+        
+        cols_for_idx = df_local.columns[model.labels_ == label_idx]
+        df_for_idx = df_local.loc[:, cols_for_idx].copy()
+        df_for_idx = pd.concat([ids, df_for_idx], axis=1)
+        df_for_idx.T.to_excel(writer, sheet_name='df{}'.format(label_idx))
+        list_df.append(df_for_idx)
+    
     writer.save()
     
     # generate 30-year plots
-    generate_30year_plots(df0, output_folder)
-    generate_30year_plots(df1, output_folder)
+    mry_helper.generate_30year_xlsx(list_df, output_dir, fname_str)
+    mry_helper.generate_30year_plots(list_df, output_dir, fname_str)
     
-    
-    
-    return(df0, df1)
+    return(list_df)
 
 
 
 
-def plot_dendrogram(df, fname):
+def plot_dendrogram(df, ml_args, output_folder, fname_str):
     
-    print('AgglomerativeClustering plus dendrograms...')
-    # affinity: {“euclidean”, “l1”, “l2”, “manhattan”, “cosine”, “precomputed”}, default euclidean
-    # linkage: {‘ward’, ‘complete’, ‘average’, ‘single’}, default=’ward’
+    df_local = df.copy()
+    
+    # Drop non-numerical columns
+    idxs_col = ['location', 'climate', 'year']
+    ids = df_local[idxs_col].copy()
+    df_local.drop(columns=idxs_col, inplace=True)
+    #print('plot_dendrogram df_local.shape, this contains all case+mp pairs:', df_local.shape)
     
     
-    # Fit model
+    # Fit model and create linkage matrix (from scikit-learn.org)
     model = AgglomerativeClustering(n_clusters=None,
-                                    affinity='manhattan',
-                                    linkage='average',
+                                    affinity=ml_args['affinity'],
+                                    linkage=ml_args['linkage'],
                                     distance_threshold=0.0)
+    model.fit(df_local.T)
     
-    model.fit(df.T)
-    
-    # Create linkage matrix (from scikit-learn.org)
     counts = np.zeros(model.children_.shape[0])
     n_samples = len(model.labels_)
     
@@ -198,85 +165,158 @@ def plot_dendrogram(df, fname):
                                      counts]).astype(float)
     
     # Plot to file
+    output_dir = os.path.join(output_folder,
+                              ml_args['name'] + '_' + ml_args['affinity'] + '_' + ml_args['linkage'])
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    
     fig, ax = plt.subplots()
     sch.dendrogram(linkage_matrix,
                    ax=ax,
                    truncate_mode='none',
                    p=3,
                    no_labels=True)
+    fname = os.path.join(output_dir,
+                         fname_str+'_dendrogram_all.png')
     fig.savefig(fname, dpi=100, bbox_inches='tight')
+    plt.close(fig)
 
     # return
     return(model)
 
 
 
+
+
+
 ################
 
+## What happens, if we select 
+# one year without filtering?
+# one year for each case and north/south orientation?
 
+# allow automatic clustering from all case+mp pairs?
+# allow automatic clustering from filtered case+mp pairs?
 
-# create X
+starts_all = [x.split('_')[0] for x in data.keys()]
+starts_unique = set(starts_all)
+print('Unique main case types:')
+print(starts_unique)
 
-case_filters = ['USP']
-y = 'M_'
-y_not = 'rank'
+# Add case+mp counts to legends
 
-df_X = create_X(data, case_filters, y, y_not)
+#starts_unique = ['UST']
 
-fname_str = '{}_{}_not{}'.format(''.join(case_filters), y, y_not)
-fname = os.path.join(output_folder,
-                     fname_str+'.png')
+# 'M_', 'RH_'
+# 'moverhygr_', 'mmass_', 'T_' & 'max' OR 'min'
+y_yes_filters = ['RH_']
+y_not_filters = ['rank', 'TRH']
 
-df0, df1 = cluster_and_export_agglomerative(df_X,
-                                            output_folder,
-                                            fname_str)
-
-model = plot_dendrogram(df_X,
-                        fname)
-
-
-# model = AgglomerativeClustering(n_clusters=2,
-#                                 affinity='manhattan',
-#                                 linkage='average')
-# model.fit(df_USP_south_M_not_rank.T)
-
-
+ml_args_agglomerative = {'name': 'agglomerative',
+                         'n_clusters': 2,
+                        'affinity': 'euclidean',
+                        'linkage': 'ward'}
 
 
 
 
+# ml_args = ml_args_agglomerative
+# affinity: {“euclidean”, “l1”, “l2”, “manhattan”, “cosine”, “precomputed”}, default euclidean
+# linkage: {‘ward’, ‘complete’, ‘average’, ‘single’}, default=’ward’
+ml_args_a_e_w = {'name': 'agglomerative',
+            'n_clusters': 2,
+            'affinity': 'euclidean',
+            'linkage': 'ward'}
+
+ml_args_a_e_a = {'name': 'agglomerative',
+            'n_clusters': 2,
+            'affinity': 'euclidean',
+            'linkage': 'average'}
+
+ml_args_a_m_a = {'name': 'agglomerative',
+            'n_clusters': 2,
+            'affinity': 'manhattan',
+            'linkage': 'average'}
+
+ml_args_k = {'name': 'kmeans',
+          'n_clusters': 2,
+          'n_init': 100,
+          'max_iter': 1000,
+          'tol': 1e-5}
+
+ml_args_d_10 = {'name': 'dbscan',
+            'eps': 10,
+            'min_samples': 1,
+            'leaf_size': 30}
+
+ml_args_d_30 = {'name': 'dbscan',
+            'eps': 30,
+            'min_samples': 1,
+            'leaf_size': 30}
+
+ml_args_d_50 = {'name': 'dbscan',
+            'eps': 50,
+            'min_samples': 1,
+            'leaf_size': 30}
+
+# spectral clustering gives "Graph is not fully connected" warnings
+#ml_args = {'name': 'spectralclustering',
+#           'n_clusters': 2}
+
+
+ml_args_all = [ml_args_a_e_w,
+               ml_args_a_e_a,
+               ml_args_a_m_a,
+               ml_args_k,
+               ml_args_d_10,
+               ml_args_d_30,
+               ml_args_d_50]
 
 
 
 
+n_clusters_max = 3
 
+for ml_args in ml_args_all:
+    print(ml_args['name'])
 
-
-
-# def plot_heatmap_corr(df, fname):
-#     # cmaps: YlGnBu, RdYlGn
+    # one value of n_clusters
+    for start in starts_unique:
+        print(' Start:', start, flush=True)
+        
+        if 'YP' in start:
+            subcategories = ['_']
+        else:
+            subcategories = ['north', 'south']
+        
+        
+        for subcategory in subcategories:
+            print('  Subcategory:', subcategory)
+        
+            case_filters = [start, subcategory]
+            df_X = mry_helper.create_X(data, case_filters, y_yes_filters, y_not_filters)
+            
+            fname_str = '{}_{}_not_{}'.format(''.join(case_filters),
+                                              ''.join(y_yes_filters),
+                                              ''.join(y_not_filters))
+            
+            # Plot filtered data to dendrogram (uses always all data of the subcategory)
+            model = plot_dendrogram(df_X,
+                                    ml_args_agglomerative,
+                                    output_folder,
+                                    fname_str)
+            
+            # Create multiple clusters from the filtered data
+            for n_clusters in range(1, n_clusters_max+1):
+                
+                ml_args['n_clusters'] = n_clusters
+                df_list = cluster_and_export(df_X,
+                                              ml_args,
+                                              output_folder,
+                                              fname_str)
     
-#     fig, ax = plt.subplots()
-#     sns.heatmap(data=df.corr(),
-#                 vmin=-1.0,
-#                 vmax=1.0,
-#                 cmap="RdYlGn",
-#                 xticklabels=False,
-#                 yticklabels=False,
-#                 cbar_kws={'shrink': 0.8,
-#                           'aspect': 30},
-#                 ax=ax)
-#     fig.savefig(fname, dpi=200, bbox_inches='tight')
-
-
-
-
-
-
-# fname = os.path.join(output_folder,
-#                      'heatmap_corr_{}_{}_{}.png'.format(''.join(case_filters), y, y_not))
-# plot_heatmap_corr(df_USP_south_M_not_rank, fname)
-
 
 
 
